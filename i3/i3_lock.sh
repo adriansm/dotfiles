b1a532e219acae707b792a098ca7951cfa28135f
Adrian Salido <adriansm@gmail.com>
2022-06-17 22:07:08 -0700

    i3: adjust auto start apps

--- i3/i3_lock.sh
+++ i3/i3_lock.sh
@@ -1,11 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-. /etc/lsb-release
-GLOCK=/usr/share/goobuntu-desktop-files/xsecurelock.sh
-if [ -f $GLOCK ]; then
+ALLOW_XSECURELOCK=1
+XSECURELOCK_BIN=/usr/share/goobuntu-desktop-files/xsecurelock.sh
+if [ $ALLOW_XSECURELOCK != 0 -a -f $XSECURELOCK_BIN ]; then
     export XSECURELOCK_SAVER=$HOME/.local/bin/saver_img
-
-    $GLOCK $@
+    . $XSECURELOCK_BIN $@
 else
     i3lock --color=4c7899 --ignore-empty-password --show-failed-attempts $@
 fi
