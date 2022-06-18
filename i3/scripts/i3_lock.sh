158a68b214518bac15aad839bdb360cdd9012b77
Adrian Salido <adriansm@gmail.com>
2022-06-17 22:07:15 -0700

    i3: misc setup

--- i3/scripts/i3_lock.sh
+++ i3/scripts/i3_lock.sh
@@ -5,6 +5,9 @@ XSECURELOCK_BIN=/usr/share/goobuntu-desktop-files/xsecurelock.sh
 if [ $ALLOW_XSECURELOCK != 0 -a -f $XSECURELOCK_BIN ]; then
     export XSECURELOCK_SINGLE_AUTH_WINDOW=1
     export XSECURELOCK_SAVER=$HOME/.local/bin/saver_img
+    export XSECURELOCK_COMPOSITE_OBSCURER=0
+    # export XSECURELOCK_DIM_TIME_MS=3000
+    # export XSECURELOCK_WAIT_TIME_MS=10000
 
     # Media Keys
     export XSECURELOCK_KEY_XF86AudioPlay_COMMAND="playerctl play-pause"
@@ -19,9 +22,10 @@ if [ $ALLOW_XSECURELOCK != 0 -a -f $XSECURELOCK_BIN ]; then
 
     . $XSECURELOCK_BIN $@
 
-    if [ -n "$XSECURE_RUN_AFTER" -a -x "$XSECURELOCK_RUN_AFTER" ]; then
-        $XSECURELOCK_RUN_AFTER
-    fi
+    # if [ -n "$XSECURE_RUN_AFTER" -a -x "$XSECURELOCK_RUN_AFTER" ]; then
+    #     $XSECURELOCK_RUN_AFTER
+    # fi
+    xrefresh
 else
     i3lock --color=4c7899 --ignore-empty-password --show-failed-attempts $@
 fi
