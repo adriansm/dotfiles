fbfc817c5328fc11a56a84d998acc15ae283607a
Adrian Salido <adriansm@gmail.com>
2022-06-17 22:07:07 -0700

    move i3 config scripts

--- i3/i3_lock.sh
+++ i3/i3_lock.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+. /etc/lsb-release
 GLOCK=/usr/share/goobuntu-desktop-files/xsecurelock.sh
-if [ -f $GLOCK ]; then
+if [ -f $GLOCK -a "$GOOGLE_ROLE" != "laptop"]; then
     export XSECURELOCK_SAVER=$HOME/.local/bin/saver_img
 
     $GLOCK $@
 else
-    i3lock $@
+    i3lock -c 333333 $@
 fi
