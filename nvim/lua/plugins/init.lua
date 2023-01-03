8967b695738b913459d5012f8610e97701078a8d
Adrian Salido <adriansm@gmail.com>
2023-01-03 13:39:41 -0800

    nvim: replace oscyank

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -188,11 +188,29 @@ local function plugins(use)
       vim.keymap.set('n', '<A-l>', splits.resize_right)
     end
   }
-  use { 'ojroques/vim-oscyank',               -- Yank text to terminal via osc52
+  -- use { 'ojroques/vim-oscyank',               -- Yank text to terminal via osc52
+  --   config = function()
+  --     vim.cmd [[autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '' | execute 'OSCYankReg "' | endif]]
+  --   end
+  -- }
+  use {
+    'ojroques/nvim-osc52',
     config = function()
-      vim.cmd [[autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '' | execute 'OSCYankReg "' | endif]]
+      require('osc52').setup {
+        max_length = 0,  -- Maximum length of selection (0 for no limit)
+        silent = false,  -- Disable message on successful copy
+        trim = false,    -- Trim text before copy
+      }
+      function copy()
+        if vim.v.event.operator == 'y' and vim.v.event.regname == '' then
+          require('osc52').copy_register('')
+        end
+      end
+
+      vim.api.nvim_create_autocmd('TextYankPost', {callback = copy})
     end
   }
+
 
 
   --
