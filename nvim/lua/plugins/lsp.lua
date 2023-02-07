9f6e778b01450ef09df32145dd943ed0f8870c45
Adrian Salido <adriansm@gmail.com>
2023-02-07 11:02:29 -0800

    nvim: disable lsp autoformat on save

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -6,6 +6,13 @@ return {
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.ensure_installed, { "lua-language-server" })
       end
+      return opts
     end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      autoformat = false,
+    },
   },
 }
