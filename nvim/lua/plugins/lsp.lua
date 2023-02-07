581994967d8902c3689d11547b7476dd8f01a0ff
Adrian Salido <adriansm@gmail.com>
2023-02-06 17:27:13 -0800

    nvim: add lsp, treesitter ensure installed

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,11 @@
+return {
+  -- tools
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "lua-language-server" })
+      end
+    end,
+  },
+}
