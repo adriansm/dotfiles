81c861665a6424cb3002fcf229251da8ffbc1329
Adrian Salido <adriansm@gmail.com>
2023-03-25 22:40:56 -0700

    update dotfiles based on recent install

--- zsh/zplug_packages.zsh
+++ zsh/zplug_packages.zsh
@@ -21,6 +21,8 @@ if [[ "$OSTYPE" =~ ^darwin ]]; then
   OH_MY_ZSH_PLUGINS+=('brew' 'osx')
 fi
 
+zplug 'zplug/zplug', hook-build:'zplug --self-manage'
+
 zplug "robbyrussell/oh-my-zsh", use:"lib/{${(j:,:)OH_MY_ZSH_LIBS}}.zsh"
 for p in $OH_MY_ZSH_PLUGINS; do
   zplug "plugins/${p}", from:oh-my-zsh
@@ -38,7 +40,8 @@ zplug "$ZSH_HOME/custom/android", from:local, use:android.plugin.zsh
 #zplug "$ZSH_HOME/custom/iterm2_integration", from:local, use:iterm2.plugin.zsh
 
 # Theme
-zplug "mafredri/zsh-async"
-zplug "$ZSH_HOME/custom/pureprompt", from:local, use:pure.zsh, as:theme
+zplug "mafredri/zsh-async", from:github
+# zplug "$ZSH_HOME/custom/pureprompt", from:local, use:pure.zsh, as:theme
+zplug sindresorhus/pure, use:pure.zsh, from:github, as:theme
 
-zplug "junegunn/fzf-bin", from:gh-r, as:command, rename-to:fzf
+# zplug "junegunn/fzf-bin", from:gh-r, as:command, rename-to:fzf
