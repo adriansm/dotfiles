7372af29b2e452d9fd50a29594525fe922681c97
Adrian Salido <adriansm@gmail.com>
2023-05-17 17:51:18 -0700

    nvim: update cmp settings

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -12,11 +12,10 @@ return {
       local luasnip = require("luasnip")
 
       return {
-        completion = {
-          autocomplete = true, -- disable auto-completion
-          completeopt = "menu,menuone,noinsert",
-        },
+        completion = opts.completion,
         mapping = cmp.mapping.preset.insert({
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
           ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
           ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
           ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
@@ -28,6 +27,10 @@ return {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
           }),
+          ["<S-CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
           ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
