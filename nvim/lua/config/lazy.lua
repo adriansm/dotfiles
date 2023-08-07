42be7d10fa1f260420cb5661e339b9dff0927b60
Adrian Salido <adriansm@gmail.com>
2023-08-07 10:49:32 -0700

    lazyvim: enable dap core

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -15,6 +15,7 @@ local options = {
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.dap.core" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
