6f7aecbccfebaff4bf7399eb0233343bebd88c97
Adrian Salido <adriansm@gmail.com>
2023-11-03 13:14:46 -0700

    nvim: add notification background

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -18,5 +18,15 @@ return {
       -- Enable italic comment
       italic_comments = true,
     },
+    config = function(_, opts)
+      local c = require('vscode.colors').get_colors()
+      opts = vim.tbl_extend("force", {
+        group_overrides = {
+          -- To make nvim-notify happy
+          NotifyBackground = { fg=c.vscFront, bg=c.vscBack },
+        },
+      }, opts or {})
+      require("vscode").setup(opts)
+    end
   },
 }
